664fe55411addf217cdf3750117935f09dc6a15f
4tqbv76qr6-wq <4tqbv76qr6@privaterelay.appleid.com>
2026-06-25 19:05:28 +0200

    Add files via upload

---
+++ jeux/arcade/alien-pulse/js/core/loop.js
@@ -0,0 +1,73 @@
+/* ============================================================
+   GAME LOOP
+   ============================================================ */
+function update() {
+    if (waitingForStart) return;
+    if (!player.alive) return;
+
+    updateStars();
+    updateExplosions();
+    player.update();
+    Bullets.update();
+    EnemyBullets.update();
+    Enemies.update();
+    Bunkers.update();
+}
+
+
+
+function draw() {
+    drawStars();
+
+    // ⭐ Test : afficher le pseudo
+    if (currentUser) {
+        ctx.fillStyle = "cyan";
+        ctx.font = "16px monospace";
+        ctx.fillText("Joueur : " + currentUser.pseudo, 10, 20);
+    }
+
+if (currentUser) {
+ currentUser.scoreGlobal = 9999;   // ⭐ TEST
+            User.saveData();                  // ⭐ Sauvegarde
+    ctx.fillStyle = "cyan";
+    ctx.font = "16px monospace";
+    ctx.fillText("Joueur : " + currentUser.pseudo, 10, 20);
+    ctx.fillText("Global : " + currentUser.scoreGlobal, 10, 40);
+ctx.fillText("Global : " + currentUser.scoreGlobal, 10, 40);
+
+}
+
+    if (!player.alive) {
+        if (!gameOverHandled) {
+            if (currentUser) {
+                //User.addScore("spaceInvader", score);
+                 User.addScore("spaceInvader", 500);
+                 currentUser.scoreGlobal = 9999;   // ⭐ TEST
+            User.saveData();                  // ⭐ Sauvegarde
+            }
+            gameOverHandled = true;
+        }
+
+        showGameOverScreen();
+        return;
+    }
+
+
+    player.draw();
+    Bunkers.draw();
+    Bullets.draw();
+    Enemies.draw();
+    EnemyBullets.draw();
+    drawExplosions();
+}
+
+
+
+    
+
+
+function loop() {
+    update();
+    draw();
+    requestAnimationFrame(loop);
+}
