98cbf815cc7b8aa7420958677b04ee0c8dacad2f
4tqbv76qr6-wq <4tqbv76qr6@privaterelay.appleid.com>
2026-06-25 19:07:20 +0200

    Add files via upload

---
+++ jeux/arcade/alien-pulse/js/engine/stars.js
@@ -0,0 +1,34 @@
+/* ============================================================
+   STARFIELD
+   ============================================================ */
+const stars = [];
+
+function initStars(count = 80) {
+    stars.length = 0;
+    for (let i = 0; i < count; i++) {
+        stars.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            size: Math.random() * 2 + 1,
+            speed: Math.random() * 0.5 + 0.2
+        });
+    }
+}
+
+function updateStars() {
+    stars.forEach(s => {
+        s.y += s.speed;
+        if (s.y > canvas.height) {
+            s.y = 0;
+            s.x = Math.random() * canvas.width;
+        }
+    });
+}
+
+function drawStars() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#0ff";
+    stars.forEach(s => ctx.fillRect(s.x, s.y, s.size, s.size));
+}
