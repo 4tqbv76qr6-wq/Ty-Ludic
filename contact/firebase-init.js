cefcfb9569420c132496c9193462fc1842d70d35
4tqbv76qr6-wq <4tqbv76qr6@privaterelay.appleid.com>
2026-07-05 19:38:41 +0200

    Update firebase-init.js

--- contact/firebase-init.js
+++ contact/firebase-init.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDODrXEPdxUIB_trqiZEFPMF5qQcZRuMyI",
   authDomain: "ty-ludic-f37a9.firebaseapp.com",
   projectId: "ty-ludic-f37a9",
-  storageBucket: "ty-ludic-f37a9.firebasestorage.app",
+  storageBucket: "ty-ludic-f37a9.appspot.com",
   messagingSenderId: "487462368828",
   appId: "1:487462368828:web:700af454a339d3b8e7f539",
   measurementId: "G-8MBXBRGXVW"
