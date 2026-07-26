3ccca2f33bee77848e8b9bb6705cb2f4adf1acb0
4tqbv76qr6-wq <4tqbv76qr6@privaterelay.appleid.com>
2026-07-26 16:13:20 +0200

    Add files via upload

--- js/ScoreManager.js
+++ js/ScoreManager.js
@@ -1,8 +1,16 @@
 // ScoreManager.js SANS export, juste pour test
 alert("ScoreManager chargé");
+alert("window.db = " + window.db);
+alert("typeof window.db = " + typeof window.db);
+alert("window.db.constructor = " + (window.db && window.db.constructor.name));
+
 
 const ScoreManager = {
     async update(gameName, newScore, pseudo) {
+alert("window.db = " + window.db);
+alert("typeof window.db = " + typeof window.db);
+alert("window.db.constructor = " + (window.db && window.db.constructor.name));
+
         alert("update() : début");
 
         alert("gameName = " + gameName);
